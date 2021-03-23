7a9adfcf289c11f26805984748379c94b44db141
vijay kag <42514875+vijayphsci@users.noreply.github.com>
2021-03-23 23:19:54 +0530

    Update test_function.py

--- fixed_point_classification/test_function.py
+++ fixed_point_classification/test_function.py
@@ -1,8 +1,10 @@
 import classify_fixed_points as cfp
 
+import classify_fixed_points as cfp
+
 def function1(x1,x2):
     return x2
 def function2(x1,x2):
-    return -x1
+    return -x1-x2
 
 cfp.classify_fixed_point_automatatic(lambda x1,x2:function1(x1,x2),lambda x1,x2:function2(x1,x2),search_time=5)
