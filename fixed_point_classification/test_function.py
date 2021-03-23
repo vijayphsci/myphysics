8c0308d882e4bca057487f283717e51d149f164d
vijay kag <42514875+vijayphsci@users.noreply.github.com>
2021-03-23 23:27:13 +0530

    Update test_function.py

--- fixed_point_classification/test_function.py
+++ fixed_point_classification/test_function.py
@@ -5,4 +5,16 @@ def function1(x1,x2):
 def function2(x1,x2):
     return -x1-x2
 
-cfp.classify_fixed_point_automatatic(lambda x1,x2:function1(x1,x2),lambda x1,x2:function2(x1,x2),search_time=5)
+
+box_size=50
+search_time=20
+detail=True
+decimal=5
+h=0.001
+epsilon=1e-6
+max_iteration=500
+
+
+
+cfp.classify_fixed_point_automatatic(function1=lambda x1,x2:function1(x1,x2),function2=lambda x1,x2:function2(x1,x2),box_size=box_size,search_time=search_time,detail=detail,decimal=decimal,h=h,epsilon=epsilon,max_iteration=max_iteration)
+
