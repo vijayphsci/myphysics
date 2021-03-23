492184161963082ad615d51eb738e1ba392511e2
vijay kag <42514875+vijayphsci@users.noreply.github.com>
2021-03-23 23:15:28 +0530

    Add files via upload

---
+++ fixed_point_classification/classify_fixed_points.py
@@ -0,0 +1,19 @@
+import numpy as np
+import fixedpointclassification as ls
+
+def classify_fixed_point(zeros,function1,function2,detail=True,h=0.001,decimal=5):
+    A=ls.parameter_matrix(zeros,function1,function2,h,decimal)
+    trace,determinant=ls.trace_and_determinant(A)
+    ans=ls.classify(trace,determinant,A,detail,decimal)
+    return ans
+
+def classify_fixed_point_automatatic(function1,function2,box_size=50,search_time=20,detail=True,decimal=5,h=0.001,epsilon=1e-6,max_iteration=500):
+    zeros=ls.non_linear_zeros_2d(function1,function2,box_size,search_time,decimal,h,epsilon,max_iteration)
+    for val in zeros:
+        x_zero,y_zero=val[0],val[1]
+        A=ls.parameter_matrix((x_zero,y_zero),function1,function2,h,decimal)
+        trace,determinant=ls.trace_and_determinant(A)
+        name='Fixed point = '+'('+str(x_zero)+' , '+str(y_zero)+')'
+        ans=ls.classify(trace,determinant,A,detail,decimal,set_title=name)
+        print(name+' : '+ans)
+    
